456156f1bff0e7878f7cfd0030aa22cf59ce71a5
cookingTorch <jhw7181@naver.com>
2024-09-26 15:22:20 +0900

    Title: 색깔 트리, Time: 173ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240926/색깔 트리/color-tree.cpp
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_ID 100001
+
+const int INF = 2147483647;
+const int QUERY100 = 100;
+const int QUERY200 = 200;
+const int QUERY300 = 300;
+const int IS_ROOT = -1;
+const int ROOT = 0;
+
+typedef struct node_s node_t;
+struct node_s {
+    int color;
+    int max_depth;
+    struct node_s *children;
+    struct node_s *next;
+};
+
+int _sum;
+node_t *_nodes[MAX_ID];
+
+node_t *
+new_node(int color, int max_depth)
+{
+    node_t *node;
+
+    node = (node_t *) malloc(sizeof(node_t));
+    node->color = color;
+    node->max_depth = max_depth;
+    node->children = NULL;
+    return node;
+}
+
+void
+add_node(node_t *parent, int m_id, int color, int max_depth)
+{
+    if (parent->max_depth == 1) {
+        return;
+    }
+    _nodes[m_id] = new_node(color, max_depth < parent->max_depth - 1 ? max_depth : parent->max_depth - 1);
+    _nodes[m_id]->next = parent->children;
+    parent->children = _nodes[m_id];
+}
+
+void
+change_color(node_t *curr, int color)
+{
+    node_t *child;
+
+    curr->color = color;
+    for (child = curr->children; child; child = child->next) {
+        change_color(child, color);
+    }
+}
+
+int
+dfs(node_t *curr) {
+    int i;
+    int cnt;
+    int colors;
+    node_t *child;
+
+    colors = 1 << curr->color - 1;
+    for (child = curr->children; child; child = child->next) {
+        colors |= dfs(child);
+    }
+    cnt = 0;
+    for (i = colors; i; i >>= 1) {
+        if (i & 1) {
+            cnt++;
+        }
+    }
+    _sum += cnt * cnt;
+    return colors;
+}
+
+int
+calc()
+{
+    node_t *child;
+
+    _sum = 0;
+    for (child = _nodes[ROOT]->children; child; child = child->next) {
+        dfs(child);
+    }
+    return _sum;
+}
+
+int
+main()
+{
+    int q;
+    int query;
+    int m_id;
+    int p_id;
+    int color;
+    int max_depth;
+
+    scanf("%d", &q);
+    _nodes[0] = new_node(0, INF);
+    while (q--) {
+        scanf("%d", &query);
+        if (query == QUERY200) {
+            scanf("%d %d", &m_id, &color);
+            change_color(_nodes[m_id], color);
+        } else if (query == QUERY100) {
+            scanf("%d %d %d %d", &m_id, &p_id, &color, &max_depth);
+            add_node(_nodes[p_id == IS_ROOT ? ROOT : p_id], m_id, color, max_depth);
+        } else if (query == QUERY300) {
+            scanf("%d", &m_id);
+            printf("%d\n", _nodes[m_id]->color);
+        } else {
+            printf("%d\n", calc());
+        }
+    }
+}
